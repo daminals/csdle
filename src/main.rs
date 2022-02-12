95a310921c80c8855c9b0d4cb3f03fc7da926b75
Daniel Kogan <54873722+daminals@users.noreply.github.com>
2022-02-11 19:10:21 -0500

    rdle

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+// Rdle
+// Daniel Kogan
+// 02.11.2022
+
+use std::env;
+use std::fs;
+
+
 fn main() {
     println!("Hello, world!");
+    let filename = "all.txt";
+
+    println!("In file {}", filename);
+
+    let contents = fs::read_to_string(filename)
+        .expect("Something went wrong reading the file");
+
+    println!("With text:\n{}", contents);
+
 }
+
