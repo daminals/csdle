18203d0cfdf50f08fd16c36fc41d682b4be73579
Daniel Kogan <54873722+daminals@users.noreply.github.com>
2022-02-21 23:11:01 -0500

    logging + investigating why code is not properly outputting yellow letters

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,32 @@
 // Daniel Kogan
 // 02.11.2022
 
+use crate::fs::File;
 use std::io;
 use std::io::Write;
 use std::fs;
 use rand::seq::SliceRandom; 
 use std::process;
+use array_tool::vec::Intersect;
+
+#[derive(Debug)]
+struct LetterPosition {
+    letter: char,
+    position: usize,
+    color: String
+}
+// helper function, returns the letter position to sort by
+fn letter_by_position(letter_pos: &LetterPosition) -> usize {
+    letter_pos.position
+}
+fn sort_LetterPosition(letter_pos: &mut Vec<LetterPosition>) {
+    letter_pos.sort_by_key(letter_by_position);
+}
 
 fn main() {
     print!("{esc}[2J{esc}[1;1H", esc = 27 as char); // clear terminal
     let solution = pick_solution();
-    //println!("{}", &solution); //don't tell people the answer lol
+    println!("{}", &solution); //don't tell people the answer lol
     for round in 1..7 {
         let guess = request_answer(round);
         let verify_guess = verify_answer(&solution, &guess, &round);
@@ -65,23 +81,20 @@ fn verify_answer(solution: &String, guess: &String, round: &u8) -> String {
     let white_background = "\u{001b}[107m";
     let clear_format = "\u{001b}[0m";
     // check guess loop
-    for (index, letter) in guess.chars().enumerate() {
-        if solution.contains(letter) && sol_remaining_letters.contains(&letter) {
-            //println!("index: {} letter: {}", index, letter);
-            //println!("{}", sol_chars[index]);
-            if sol_chars[index] == letter {
-                // return letter as green
-                computed_answer += &format!("{} {}{} {}", &green_background, &black_text, letter, &clear_format).to_owned();
-            } else {
-                // return letter as yellow
-                computed_answer +=  &format!("{} {}{} {}", &yellow_background, &black_text, letter, &clear_format).to_owned();
-            }
-            let remove_letter = sol_remaining_letters.iter().position(|x| *x == letter).unwrap();
-            sol_remaining_letters.remove(remove_letter);    
-        } else {
-            computed_answer += &format!("{} {}{} {}", &white_background, &black_text, letter, &clear_format).to_owned();
-        }
+    
+    let color_guess = return_green_letters(&guess, &solution);
+    //println!("{:?}", color_guess);
+    let color_guess = return_gray_letters(&guess, &solution, color_guess);
+    let mut color_guess = return_yellow_letters(&guess, &solution, color_guess);
+    sort_LetterPosition(&mut color_guess);
+
+    let mut computed_answer = "".to_string();
+    for item in color_guess {
+        let col: &str = &String::from(item.color).to_owned()[..];
+        let letter: &str = &String::from(item.letter).to_owned()[..];
+        computed_answer += &format!("{}{} {} {}", col, &black_text, letter, &clear_format).to_owned()[..];
     }
+
     if guess == solution {
         println!("{}", computed_answer);
         println!("Yay! You have won in {} guesses!!", round);
@@ -117,4 +130,60 @@ fn is_in_file(guess: &String, filename: &str) -> bool {
         .expect("Something went wrong reading the file");
     let all_words = contents.lines().collect::<Vec<_>>();
     return all_words.contains(&&guess.to_owned()[..]);
+}
+fn return_green_letters(guess: &str, solution: &str) -> Vec<LetterPosition> {
+    let sol_chars = solution.chars().collect::<Vec<_>>();
+    let mut correct_letters = vec![];
+    for (index, letter) in guess.chars().enumerate() {
+        if sol_chars[index] == letter {
+            correct_letters.push(LetterPosition{letter: letter, position: index, color: "\u{001b}[42m".to_string()});
+        }
+    }
+    return correct_letters;
+}
+fn return_gray_letters(guess: &str, solution: &str, mut gray_letters: Vec<LetterPosition>)  -> Vec<LetterPosition> {
+    let sol_chars = solution.chars().collect::<Vec<_>>();
+    for (index, letter) in guess.chars().enumerate() {
+        if !sol_chars.contains(&letter) {
+            gray_letters.push(LetterPosition{letter: letter, position: index, color: "\u{001b}[107m".to_string()});
+        }
+    }
+    return gray_letters;
+}
+fn return_yellow_letters(guess: &str, solution: &str, mut yellow_letters: Vec<LetterPosition>) -> Vec<LetterPosition> {
+    let mut logs = File::create("logs.txt").unwrap();
+    
+    let mut remaining_letters = guess.chars().collect::<Vec<char>>();
+    let sol_chars = solution.chars().collect::<Vec<char>>();
+    writeln!(&mut logs, "green + gray array {:?}", yellow_letters);
+    writeln!(&mut logs, "initial array {:?}", yellow_letters);
+    let mut charr_array: Vec<char> = vec![];
+    for letter in &yellow_letters {
+        charr_array.push(letter.letter);
+    }
+    remaining_letters = remaining_letters.intersect(charr_array);
+    let final_gray_letters = remaining_letters.clone();
+    let mut remaining_letters = remaining_letters.intersect(sol_chars);
+    let sol_chars = solution.chars().collect::<Vec<char>>();
+    writeln!(&mut logs, "new array {:?}", remaining_letters);
+
+    //println!("{:?}", guess.chars().enumerate());
+
+    for (index, letter) in guess.chars().enumerate() {
+        //println!("{}", letter);
+        //println!("{:?}", remaining_letters);
+        if remaining_letters.contains(&letter) && solution.contains(letter) && (sol_chars[index] != letter) {
+            yellow_letters.push(LetterPosition { letter: letter, position: index, color: "\u{001b}[43m".to_string() } );
+            remaining_letters = drop_letter(&mut remaining_letters, letter);
+        } else if final_gray_letters.contains(&letter) && solution.contains(letter) && (sol_chars[index] != letter) {
+            yellow_letters.push(LetterPosition { letter: letter, position: index, color: "\u{001b}[107m".to_string() } );
+        }
+    }
+    println!("{:?}", yellow_letters);
+    return yellow_letters;
+}
+fn drop_letter(char_array: &mut Vec<char>, letter: char) -> Vec<char> {
+    let remove_letter = char_array.iter().position(|x| *x == letter).unwrap();
+    char_array.remove(remove_letter);    
+    return char_array.to_vec();
 }
